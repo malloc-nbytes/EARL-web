c587e3aa159ee240078123db6839b58981ab3b79
malloc-nbytes <zdhdev@yahoo.com>
2024-10-20 02:16:41 -0500

    more styling

---
+++ src/components/EARLInfoNote.jsx
@@ -0,0 +1,9 @@
+function EARLInfoIndent({ children }) {
+    return (
+        <div className="p-4 bg-gray-800 border-l-4 border-blue-500 shadow-md rounded-md">
+            {children}
+        </div>
+    );
+}
+
+export default EARLInfoIndent;
