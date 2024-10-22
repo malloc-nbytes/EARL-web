e5d5ab050d4c37fb0cad8a68f213b0632da737b0
malloc-nbytes <zdhdev@yahoo.com>
2024-10-22 16:17:47 -0500

    added basename

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://malloc-nbytes.github.io/EARL-web/",
   build: {
     outDir: 'build',
   }
