5dc0c9a057da2465cd4da89800962a412b4f50f1
malloc-nbytes <zdhdev@yahoo.com>
2024-10-22 16:22:22 -0500

    restored basename

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,17 @@ import InvalidPage from "./pages/InvalidPage";
 
 function App() {
     return (
-        <Routes>
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/documentation" element={<Documentation />} />
-            <Route path="/examples" element={<Examples />} />
-            <Route path="*" element={<InvalidPage />} />
-        </Routes>
+        <BrowserRouter basename="/EARL-web">
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/documentation" element={<Documentation />} />
+                <Route path="/examples" element={<Examples />} />
+                <Route path="*" element={<InvalidPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
