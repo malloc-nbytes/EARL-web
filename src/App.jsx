bc39b38593691935dcf25fc7b906493b821ee0ac
malloc-nbytes <zdhdev@yahoo.com>
2024-10-22 16:12:26 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,24 @@ import Examples from "./pages/Examples";
 import Home from "./pages/Home";
 import InvalidPage from "./pages/InvalidPage";
 
-function Doxygen() {
-    window.location.href = "./doxygen/index.html";
-}
+// function Doxygen() {
+//     window.location.href = "./doxygen/index.html";
+// }
+// <Route path="/source" element={<Doxygen />} />
 
 function App() {
     return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/documentation" element={<Documentation />} />
-                    <Route path="/examples" element={<Examples />} />
-                    <Route path="/source" element={<Doxygen />} />
-                    <Route path="*" element={<InvalidPage />} />
-                </Routes>
-            </BrowserRouter>
-        </>
+        <BrowserRouter basename="/EARL-web">
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/documentation" element={<Documentation />} />
+                <Route path="/examples" element={<Examples />} />
+                <Route path="*" element={<InvalidPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
